918c2eb499f613bd2f6608b3c41acb7e2077929c
TiaBroch <tiabroch@gmail.com>
2023-03-01 17:37:33 +0100

    n and top_n are checked if number, some advancement with logging

--- Server/API_backend.py
+++ Server/API_backend.py
@@ -1,4 +1,3 @@
-
 from Server import show_leaderboard
 from Server import utiliy
 from Server.server_manager import server
@@ -12,10 +11,10 @@ def insert_eid_api(EID,mongo):
         server_manager = server()
         result = server_manager.get_bot_first_contact(EID)
         if not result:
-            return {"success": False, "code": -3, "content": "Unable to contact egg server"}
+            return {"success": False, "code": -3, "content": "EID not present on the server"}
         checksum = result.backup.checksum
         if checksum == 0:
-            return {"success":False,"code":-2,"content":""}
+            return {"success":False,"code":-2,"content":"EID is not valid for some reason"}
         else:
             name=result.backup.user_name
 
@@ -26,16 +25,27 @@ def insert_eid_api(EID,mongo):
 
 def get_leaderboard(mongo, element, n, top_n):
     logging.basicConfig(filename='getLeaderboard.log', level=logging.INFO)
-
     try:
-        logging.info('Called get_leaderboard with parameter: '+str(element) if element is not None else "" +" : "+str(n) if n is not None else ""+" : "+str(top_n)if top_n is not None else "")
         if element is None or n is None or top_n is None:
             return {"success": False, "code": 0, "content": "Malformed get"}
+        if not n.isnumeric()  or not top_n.isnumeric():
+            return {"success": False, "code": 0, "content": "Malformed get"}
+        logging.info('Called get_leaderboard with parameter: '+str(element)+" : "+str(n)+" : "+str(top_n))
         if mongo is None:
             return {"success": False, "code": -4, "content": "Unable to contact mongo"}
         if str(element) not in utiliy.get_leaderboards_names_static():
             return {"success": False, "code": 0, "content": str(element)+" is not a valid leaderboard"}
-        return {"success": True, "code": 1, "content": show_leaderboard.get_leader(mongo, str(element), int(n), int(top_n))}
+
+
+
+        res=show_leaderboard.get_leader(mongo, str(element), int(n), int(top_n))
+        if type(res) is not Exception:
+            logging.info(
+                'Get_leaderboard with parameter: ' + str(element) + " : " + str(
+                    n) + " : " + str(top_n)+" returned OK")
+            return {"success": True, "code": 1, "content": res}
+        else:
+            logging.warning(res)
+            return {"success": False, "code": -1, "content": str(res)}
     except Exception as e:
-        logging.critical(e)
         return {"success": False, "code": -1, "content": str(e)}
