2ffbfaf602e33bf259e2a79fef3f1d73bbf26846
TiaBroch <tiabroch@gmail.com>
2023-04-13 12:52:37 +0200

    Added method to refresh leaderboard, todo git action on it

---
+++ main_debug.py
@@ -0,0 +1,16 @@
+from Server import server_manager
+from Server.mongoDB_manager import mongo_manager
+from Server import API_backend
+
+user=os.environ.get('user')
+pssw=os.environ.get('pssw')
+
+
+conn = "mongodb+srv://" + user + ":" + pssw + "@eggcluster.sbrsi.mongodb.net/?retryWrites=true&w=majority"
+
+
+mongo = mongo_manager(conn)
+server=server_manager.server()
+
+API_backend.debug_only_reset_leaderboards(mongo)
+
